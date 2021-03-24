2ae974ba6fa55ca2422cd4c2f1a1c6286e183f5c
DanielSarkwa <dannysark16@gmail.com>
2021-03-23 16:48:44 -0800

    added logging 2 to orders service

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,6 +7,7 @@ import { ExpirationCompleteListener } from './events/listeners/expiration-comple
 
 const start = async () => {
   console.log('starting up......');
+  console.log('starting up 222......');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
